8d8e6222871ea22523ec5f259a96ea55e35df861
xGentuso <ryan.mota@triosstudent.com>
2024-11-14 10:18:26 -0500

    updated styling

--- Forcastly/src/app/search/search.component.ts
+++ Forcastly/src/app/search/search.component.ts
@@ -17,21 +17,38 @@ export class SearchComponent {
     this.searchControl.valueChanges.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap(query => this.locationService.getLocations(query))
+      switchMap(query => {
+        if (typeof query === 'string') {
+          return this.locationService.getLocations(query);
+        }
+        return [];
+      })
     ).subscribe(results => {
       this.locations = results;
     });
   }
 
   onOptionSelected(location: any) {
-    this.searchEvent.emit(location.name);
+    if (location && location.name) {
+      this.searchEvent.emit(location.name);
+    }
   }
 
   getWeather() {
-    // Add your weather fetching logic here
+    const value = this.searchControl.value;
+    if (value) {
+      // If value is an object (location selected from autocomplete)
+      if (typeof value === 'object' && value.name) {
+        this.searchEvent.emit(value.name);
+      }
+      // If value is a string (manually typed)
+      else if (typeof value === 'string') {
+        this.searchEvent.emit(value);
+      }
+    }
   }
 
-  displayFn(option: any): string {
-    return option ? option.name : '';
+  displayFn(location: any): string {
+    return location ? location.name : '';
   }
 }
